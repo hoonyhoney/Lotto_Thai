6618e8c76c1a931ae10882248ae7f0bc2b7abafc
mattang123 <hoonyhoeny@gmail.com>
2020-04-20 13:13:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lotto_thai/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.lotto_thai
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        randomCard.setOnClickListener(){
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putIntegerArrayListExtra("result", ArrayList(LottoNumberMaker.getShuffleLottoNumbers()))
+            startActivity(intent)
+        }
+
+        constellationCard.setOnClickListener(){
+            startActivity(Intent(this, ConstellationActivity::class.java))
+        }
+
+        nameCard.setOnClickListener() {
+            startActivity(Intent(this, NameActivity::class.java))
+
+        }
+
+    }
+}
